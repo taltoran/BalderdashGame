b6c985b110b622f5d1653557cfe41e263636fe70
taltoran <deepnsnow@gmail.com>
2017-03-27 16:20:41 -0600

    latests updates adding requirement of socket.io in the app.js file

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var socketio = require('socket.io');
 
 var hbs = require('express-handlebars');
 
