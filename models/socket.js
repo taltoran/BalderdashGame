91d7f0e6a8de76012283e0b2c3586bcb2a0d0dbd
bradyadair <coolguy043@hotmail.com>
2017-03-29 15:46:59 -0600

    loops game. Still needs actual questions, and rooms desperately.

--- models/socket.js
+++ models/socket.js
@@ -3,6 +3,14 @@ module.exports = (io) => {
     var app = require('express');
     var router = app.Router();
     var userCount = 0;
+    var userPointsDict = {};
+    var userAnswersDict = {};
+    var myScoresHtml = ""; 
+    var answersDisplayed = 0;
+
+    //how many users have answered question.
+    var usersAnswered = 0;
+
     // socket.io events
     io.on('connection', function (socket) {
         userCount++;
@@ -13,6 +21,54 @@ module.exports = (io) => {
             username: 'Chat It Up', 
             text: 'Welcome to Chat', 
         });*/ //Brady took out
+
+        socket.on('emptyUserAnswers', function (msg) {
+            userAnswersDict = {}
+
+            
+            console.log("emptyUserAnswers "+userAnswersDict[msg.text] + " ,answer username: " +msg.username);
+            
+            if (answersDisplayed == 0)
+            {
+                    answersDisplayed += 1;
+
+                myScoresHtml = "<h1>Points Scored During the Round</h1>"+myScoresHtml
+                myScoresHtml += "<h1> Total Scores: </h1>";
+                
+                for (var key in userPointsDict) {
+                    var value = userPointsDict[key];
+                    // Use `key` and `value`
+                    myScoresHtml += "<p>" +key +" points: " + value +"</p>";
+                        
+                }
+            }
+            
+                
+            
+            
+            console.log("I'm about to show scores");
+            io.emit('showScores', { 
+                text: myScoresHtml
+            });
+
+            //myScoresHtml = "";
+        });
+
+        socket.on('addToChosenAnswer', function (msg) {
+
+            //first give user points 
+            userPointsDict[userAnswersDict[msg.text]] += 1;
+
+            console.log("<p>"+userAnswersDict[msg.text] + " points: " +userPointsDict[userAnswersDict[msg.text]] +"</p>");
+
+            myScoresHtml +="<p>"+msg.username + " picked the answer " + msg.text+ " so "+userAnswersDict[msg.text] + " scored 1 point</p>";//" +userPointsDict[userAnswersDict[msg.text]] +" 
+
+            io.emit('addOneChosenAnswer', { 
+                 usersCount: userCount
+            });
+        });
+       
+
         socket.on('send', function (msg) {
             var stamp = new Date().toLocaleTimeString();
             if(stamp.length == 10 ){
@@ -27,16 +83,37 @@ module.exports = (io) => {
                 time: stamp
             });
             */
+
+            myScoresHtml = "";
+            answersDisplayed = 0;
+
+            usersAnswered += 1;
+
+            if (usersAnswered == userCount)
+            {
+                io.emit('hideLoading', { 
+                });
+
+                usersAnswered = 0;
+            }
+
+            
             
             //Brady Added
             io.emit('answerMessage', { 
                 username: msg.username, 
                 text: msg.text, 
-                time: stamp
+                time: stamp,
+                //usersCount: userCount
             });
+
+            userAnswersDict[msg.text] = msg.username;
+
         });
 
         socket.on('sendQuestion', function (msg) {
+
+            
             
             //Brady Added
             io.emit('questionMessage', { 
@@ -52,11 +129,18 @@ module.exports = (io) => {
             }else{
                 stamp = stamp.substring(0, 5) + ' ' + stamp.substring(9, 11)
             }
+
+            userPointsDict[msg.username] = 0;
+
+            console.log(msg.username + " answer: " +userAnswersDict[msg.username]);
+
+
             io.emit('message', {
                 //username: 'Chat It Up', //Brady took this out
                 text: msg.username + ' has joined Room: '+ userCount + ' Users are logged in.',//Chat',  //Brady changed this
                 time: stamp
             });
+
             //Brady added to check if it's the host, and set the host name
             if (userCount == 1)
             {
